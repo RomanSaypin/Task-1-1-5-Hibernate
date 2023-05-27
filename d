f47226ff8055d890b9bcc75f5cb13e7b7fa23c65
RomanSaypin <roman9126117@gmail.com>
2023-05-26 19:45:20 -0700

    remade class UserDaoJDBCImpl and class Main

---
+++ d
@@ -0,0 +1,23 @@
+[33mcommit a1fc02e0edab222a551c83e060743b9f66e02dc2[m[33m ([m[1;36mHEAD -> [m[1;32mTask1/1/4/Heber[m[33m)[m
+Author: RomanSaypin <roman9126117@gmail.com>
+Date:   Fri May 26 14:13:51 2023 -0700
+
+     задача в работе
+
+[33mcommit a00f9e5c95e6623c0d134b400af2795e819f72b9[m[33m ([m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m, [m[1;32mmaster[m[33m)[m
+Author: RomanSaypin <roman9126117@gmail.com>
+Date:   Fri May 26 13:16:49 2023 -0700
+
+     задача выполненна
+
+[33mcommit 411e0c24ac473952d9bf74783a532b9fd4fe645e[m
+Author: Bolotov Alexander <distalker1@gmail.com>
+Date:   Mon Jan 17 13:52:50 2022 +0300
+
+    pom fix
+
+[33mcommit 2c048dffcff1120b505d83e62c8b3672c1155f8c[m
+Author: nikitanesterenkos@gmail.com <piramida741147>
+Date:   Wed Jun 10 14:42:16 2020 +0300
+
+    Initial commit
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -10,44 +10,35 @@ public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
 
     }
-
+    @Override
     public void createUsersTable() {
-        Connection connection = Util.getConnection();
         String requestSQLCreateUsersTable = "CREATE TABLE IF NOT EXISTS user(id INT PRIMARY KEY AUTO_INCREMENT, " +
                 "name_user VARCHAR(40)," +
                 "lastName_user VARCHAR(40)," +
                 "age INT)";
-        try (Statement statement = connection.createStatement()) {
+        try ( Connection connection = Util.getConnection();
+              Statement statement = connection.createStatement()) {
             statement.execute(requestSQLCreateUsersTable);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
-
+    @Override
     public void dropUsersTable() {
-        Connection connection = Util.getConnection();
         String requestSQLDropUsersTable = "DROP TABLE IF EXISTS user";
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
             statement.executeUpdate(requestSQLDropUsersTable);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
-
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-        Connection connection = Util.getConnection();
+
         String requestSQLSaveUser = "INSERT INTO user(name_user, lastName_user, age) VALUES(?,?,?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(requestSQLSaveUser)) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(requestSQLSaveUser)) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setInt(3, age);
@@ -56,33 +47,24 @@ public class UserDaoJDBCImpl implements UserDao {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
+    }
+    @Override
     public void removeUserById(long id) {
-        Connection connection = Util.getConnection();
         String requestSQLRemoveUserById = "DELETE FROM user WHERE id = " + id;
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
             statement.executeUpdate(requestSQLRemoveUserById);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
-
+    @Override
     public List<User> getAllUsers() {
-        Connection connection = Util.getConnection();
         String requestSQLGetAllUsers = "SELECT * FROM user";
         List<User> list = new ArrayList<>();
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(requestSQLGetAllUsers);
             while (resultSet.next()) {
                 User user = new User();
@@ -95,26 +77,15 @@ public class UserDaoJDBCImpl implements UserDao {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
         System.out.println(list);
         return list;
-
     }
-
+    @Override
     public void cleanUsersTable() {
-        Connection connection = Util.getConnection();
         String requestSQLCleanUsersTable = "DELETE FROM user";
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
             statement.executeUpdate(requestSQLCleanUsersTable);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            connection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
