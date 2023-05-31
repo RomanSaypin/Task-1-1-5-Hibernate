d5380d48bac508021cace14c6fa5fbe10a026512
RomanSaypin <roman9126117@gmail.com>
2023-05-30 17:22:46 -0700

    Исправил

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    private static Connection connection;
+
     private static SessionFactory factory;
     private static final String URL = "jdbc:mysql://localhost:3306/database";
     private static final String USER_NAME = "root";
@@ -21,37 +21,40 @@ public class Util {
     private static final String DIAL = "org.hibernate.dialect.MySQLDialect";
 
     public static SessionFactory getSessionFactory() {
-        try {
-            Configuration configuration = new Configuration();
-            Properties settings = new Properties();
-            settings.put(Environment.URL, URL);
-            settings.put(Environment.USER, USER_NAME);
-            settings.put(Environment.PASS, PASSWORD);
-            settings.put(Environment.DIALECT, DIAL);
-            
-            settings.put(Environment.SHOW_SQL, "true");
+        if (factory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USER_NAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, DIAL);
 
-            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.SHOW_SQL, "true");
 
-            settings.put(Environment.HBM2DDL_AUTO, "");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-            configuration.setProperties(settings);
+                settings.put(Environment.HBM2DDL_AUTO, "");
 
-            configuration.addAnnotatedClass(User.class);
+                configuration.setProperties(settings);
 
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
+                configuration.addAnnotatedClass(User.class);
 
-            factory = configuration.buildSessionFactory(serviceRegistry);
-        } catch (Exception e) {
-            System.out.println("Соеденение отсутствует!");
-            throw new RuntimeException(e);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                factory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                System.out.println("Соеденение отсутствует!");
+                throw new RuntimeException(e);
+            }
         }
         return factory;
     }
 
     public static Connection getConnection() {
-        try  {
+        Connection connection;
+        try {
             connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
         } catch (SQLException e) {
             throw new RuntimeException(e);
